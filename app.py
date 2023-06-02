c1cc7a1b908c0e63876a5be5d7055ff1e28d641e
Derrick Dadson <derrickdadson@Derricks-Macbook.local>
2023-06-02 10:59:58 +0000

    postman testing

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def append_message(role, content):
 
 @app.route('/api/chatbot', methods=['POST'])
 def chatbot_response():
-    message = request.form.get('message')
-    session_id=request.form.get('session_id')
+    message = request.json.get('message')
+    session_id=request.json.get('session_id')
 
     if 'session_id' not in session or session['session_id'] != session_id:
         create_session(session_id)
